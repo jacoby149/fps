c49f54e6b14399a0bba5d0b672f219975fd61ca6
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-05-05 14:14:46 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import NavItem from "./components/NavItem";
+import './index.css';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
 
 function App() {
   return (
-    <div> Hello! </div>
+    <div>
+      <Navbar>
+      
+        <NavItem icon='shape'/>
+        <NavItem icon='color'/>
+        <NavItem>
+          <p>Hello World</p>
+        </NavItem>
+       
+        <NavItem icon={ <CogIcon />}>
+        {/* <DropdownMenu></DropdownMenu> */}
+      </NavItem>
+
+       </Navbar>
+
+      
+    </div>
   );
 }
 
